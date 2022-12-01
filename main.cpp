a2b6dcdcfa3fbea83ff38cddde2e475ca81dcb3a
Richard <richard232manansala@gmail.com>
2022-12-01 19:25:59 +0800

    remove unused defined key constants

--- main.cpp
+++ main.cpp
@@ -177,8 +177,6 @@ string playerName = "UNDEFINED";
 
 #define KEY_UP 72
 #define KEY_DOWN 80
-#define KEY_LEFT 75
-#define KEY_RIGHT 77
 #define KEY_ENTER 13
 #define KEY_SPACE 32
 #define KEY_W 119
