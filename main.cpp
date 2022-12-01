3d5341de76335a8d87a500e9b4190933935d1fa2
Richard <richard232manansala@gmail.com>
2022-12-01 20:38:38 +0800

    ran formatter and manual formatted some lines

--- main.cpp
+++ main.cpp
@@ -3,135 +3,141 @@
 #include <algorithm>
 #include <random>
 #include <chrono>
-#include<conio.h>
+#include <conio.h>
 #include <windows.h>
-
 
 using namespace std;
 
 const bool DEBUG = false;
 
-
-template<typename T>
+template <typename T>
 std::vector<T> create_copy(std::vector<T> const &vec)
 {
     std::vector<T> v;
     v.assign(vec.begin(), vec.end());
     return v;
 }
-template<typename T>
-void shuffle_vector(vector<T> &vec) {
+template <typename T>
+void shuffle_vector(vector<T> &vec)
+{
     unsigned seed = std::chrono::system_clock::now()
                         .time_since_epoch()
                         .count();
     shuffle(vec.begin(), vec.end(), default_random_engine(seed));
 }
 
-void MSleep(int ms) {Sleep(ms); }
+void MSleep(int ms) { Sleep(ms); }
 
-vector<string> splitStringToLines(string h) {
+vector<string> splitStringToLines(string h)
+{
     vector<string> arr;
     string nice = "";
-    for(unsigned int i =0; i < h.length(); i++) {
+    for (unsigned int i = 0; i < h.length(); i++)
+    {
         char c = h[i];
-        if(c == '\n') {
+        if (c == '\n')
+        {
             arr.push_back(nice);
             nice = "";
         }
-        nice+=c;
+        nice += c;
     }
     return arr;
 }
 
-class CoutAnimation {
-    public:
-        bool isAnimating=true;
-        void cancel() {isAnimating = false;}
+class CoutAnimation
+{
+public:
+    bool isAnimating = true;
+    void cancel() { isAnimating = false; }
 };
 
-struct Question {
-    public:
-        string question;
-        string correctAnswer;
-        vector<string> wrongAnswers;
+struct Question
+{
+public:
+    string question;
+    string correctAnswer;
+    vector<string> wrongAnswers;
 
-        Question(
-                 string q, string correctAnswer, initializer_list<string> wrongAnswers
-        ):
-            question(q),
-            correctAnswer(correctAnswer),
-            wrongAnswers(wrongAnswers){};
-        Question(
-                 string q, string correctAnswer, vector<string> wrongAnswers
-        ):
-            question(q),
-            correctAnswer(correctAnswer),
-            wrongAnswers(wrongAnswers){};
-        Question() {}
+    Question(
+        string q, string correctAnswer, initializer_list<string> wrongAnswers) : question(q),
+                                                                                 correctAnswer(correctAnswer),
+                                                                                 wrongAnswers(wrongAnswers){};
+    Question(
+        string q, string correctAnswer, vector<string> wrongAnswers) : question(q),
+                                                                       correctAnswer(correctAnswer),
+                                                                       wrongAnswers(wrongAnswers){};
+    Question() {}
 
-
-    vector<string> getChoices(bool randomize=true) {
+    vector<string> getChoices(bool randomize = true)
+    {
         auto choices = create_copy(wrongAnswers);
         choices.push_back(correctAnswer);
-        if(randomize) {
+        if (randomize)
+        {
             shuffle_vector(choices);
-
         }
         return choices;
     }
-
 };
 
+struct QuestionHistory : public Question
+{
+public:
+    QuestionHistory(
+        Question q,
+        int chosenAnswerIndex,
+        vector<string> choices) : Question(q.question, q.correctAnswer, q.wrongAnswers)
+    {
+        this->choices = choices;
+        this->chosenAnswerIndex = chosenAnswerIndex;
+    }
 
-
-
-struct QuestionHistory : public Question {
-    public:
-        QuestionHistory(
-             Question q,
-             int chosenAnswerIndex,
-             vector<string> choices
-        ) : Question(q.question, q.correctAnswer, q.wrongAnswers) {
-            this-> choices = choices;
-            this->chosenAnswerIndex = chosenAnswerIndex;
+    vector<string> choices;
+    int chosenAnswerIndex;
+    bool GotCorrectAnswer()
+    {
+        return choices[chosenAnswerIndex] == correctAnswer;
+    }
+    int GetScore()
+    {
+        if (GotCorrectAnswer())
+        {
+            return 1;
         }
-
-
-        vector<string>  choices;
-        int chosenAnswerIndex;
-        bool GotCorrectAnswer() {
-            return choices[chosenAnswerIndex] == correctAnswer;
-        }
-        int GetScore() {
-            if(GotCorrectAnswer()) {
-                return 1;
-            }
-            return 0;
-        }
+        return 0;
+    }
 };
 
-
-
-bool validateInput(int input) {
-    if(input <= 0 || input > 4) {
+bool validateInput(int input)
+{
+    if (input <= 0 || input > 4)
+    {
         return false;
     }
     return true;
 }
 
-void clear_input() {
+void clear_input()
+{
     cin.clear();
-    while(cin.get() != '\n') {}
+    while (cin.get() != '\n')
+    {
+    }
 }
 
-void acout(string h,bool skippable =true,int ms=20) {
+void acout(string h, bool skippable = true, int ms = 20)
+{
     int index = 0;
-    for(char c : h) {
+    for (char c : h)
+    {
         cout << c;
         index++;
-        if(!DEBUG) {
+        if (!DEBUG)
+        {
             MSleep(ms);
-            if(skippable && _kbhit()) {
+            if (skippable && _kbhit())
+            {
                 getch();
                 string rest = h.substr(index);
                 cout << rest;
@@ -141,61 +147,60 @@ void acout(string h,bool skippable =true,int ms=20) {
     }
 }
 
-void clear_screen() {
+void clear_screen()
+{
     system("CLS");
 }
 
-void fakeLoading(string message, int ms=1500) {
+void fakeLoading(string message, int ms = 1500)
+{
     const int MS = 100;
-    int loops = (ms/MS);
+    int loops = (ms / MS);
     int dots = 0;
-    while(loops > 0) {
+    while (loops > 0)
+    {
         clear_screen();
         cout << message;
-        for(int i=0; i < dots; i++) {
+        for (int i = 0; i < dots; i++)
+        {
             cout << ".";
         }
         dots++;
-        if(dots == 5) {
+        if (dots == 5)
+        {
             dots = 0;
         }
         loops--;
-        if(!DEBUG) {
+        if (!DEBUG)
+        {
             MSleep(MS);
         }
     }
 }
 
-
 vector<Question> easyQuestions = {
     Question("Easy Who invented the first calculator?", "Correct Answer", {"Wrong", "Eminem", "Dwayne Johnson"}),
     Question("Easy Who invented the second calculator?", "Correct Answer", {"Jackie Chad", "Willie Wonka", "Dwayne Johnson"}),
     Question("Easy Who invented the third calculator?", "Correct Answer", {"Luffy Uzumaki", "Blue", "The rock"}),
-    Question("Easy Who invented the last calculator?", "Correct Answer", {"Nice Person", "Bruce", "Dwayne Lee"})
-};
-
+    Question("Easy Who invented the last calculator?", "Correct Answer", {"Nice Person", "Bruce", "Dwayne Lee"})};
 
 vector<Question> mediumQuestions = {
     Question("Medium Who invented the first calculator?", "Correct Answer", {"Wrong", "Eminem", "Dwayne Johnson"}),
     Question("Medium Who invented the second calculator?", "Correct Answer", {"Jackie Chad", "Willie Wonka", "Dwayne Johnson"}),
     Question("Medium Who invented the third calculator?", "Correct Answer", {"Luffy Uzumaki", "Blue", "The rock"}),
-    Question("Medium Who invented the last calculator?", "Correct Answer", {"Nice Person", "Bruce", "Dwayne Lee"})
-};
+    Question("Medium Who invented the last calculator?", "Correct Answer", {"Nice Person", "Bruce", "Dwayne Lee"})};
 
 vector<Question> hardQuestions = {
     Question("Hard Who invented the first calculator?", "Correct Answer", {"Wrong", "Eminem", "Dwayne Johnson"}),
     Question("Hard Who invented the second calculator?", "Correct Answer", {"Jackie Chad", "Willie Wonka", "Dwayne Johnson"}),
     Question("Hard Who invented the third calculator?", "Correct Answer", {"Luffy Uzumaki", "Blue", "The rock"}),
-    Question("Hard Who invented the last calculator?", "Correct Answer", {"Nice Person", "Bruce", "Dwayne Lee"})
-};
-
+    Question("Hard Who invented the last calculator?", "Correct Answer", {"Nice Person", "Bruce", "Dwayne Lee"})};
 
 string prefixes[4] = {"1. ", "2. ", "3. ", "4. "};
 
 vector<QuestionHistory> history = {};
 
 string playerName = "UNDEFINED";
-
 
 #define KEY_UP 72
 #define KEY_DOWN 80
@@ -207,77 +212,88 @@ string playerName = "UNDEFINED";
 const std::string reset("\033[32;40m");
 const std::string magenta("\033[0;35m");
 
-int interactiveInput(string label,vector<string> choices, string endLabel="Press ENTER/SPACE to select. Arrow keys to move. \n") {
-    int _index=0;
+int interactiveInput(string label, vector<string> choices, string endLabel = "Press ENTER/SPACE to select. Arrow keys to move. \n")
+{
+    int _index = 0;
     int c = 0;
     bool looping = true;
     int choiceIndex = 0;
-    int maxNum = choices.size()-1;
+    int maxNum = choices.size() - 1;
 
     clear_screen();
     acout(label + "\n\n");
     MSleep(75);
-    for(string h : choices) {
-        if(_index == choiceIndex) {
+    for (string h : choices)
+    {
+        if (_index == choiceIndex)
+        {
             cout << magenta;
-            cout<<"=>";
-        } else {
-            cout<< "  ";
+            cout << "=>";
         }
-        cout<< h;
+        else
+        {
+            cout << "  ";
+        }
+        cout << h;
         cout << reset;
 
-        cout<< "\n";
+        cout << "\n";
         choiceIndex++;
-    MSleep(75);
+        MSleep(75);
     }
     acout("\n" + endLabel);
-    while(looping) {
+    while (looping)
+    {
         choiceIndex = 0;
         clear_screen();
-        cout << label << endl << endl;
+        cout << label << endl
+             << endl;
 
-        for(string h : choices) {
-            if(_index == choiceIndex) {
+        for (string h : choices)
+        {
+            if (_index == choiceIndex)
+            {
                 cout << magenta;
                 cout << "=>";
-            } else {
+            }
+            else
+            {
                 cout << "  ";
             }
             cout << h;
-
             cout << reset;
-
             cout << endl;
             choiceIndex++;
         }
-        cout << endl << endLabel;
+        cout << endl
+             << endLabel;
 
-        switch(c=getch()) {
-        case KEY_UP:
-        case KEY_W:
-            _index--;
-            if(_index < 0) {
-                _index = maxNum;
-            }
-            break;
-        case KEY_DOWN:
-        case KEY_S:
-            _index++;
-            if(_index > maxNum) {
-                _index = 0;
-            }
-            break;
-        case KEY_ENTER:
-        case KEY_SPACE:
-            looping = false;
-            break;
-
+        switch (c = getch())
+        {
+            case KEY_UP:
+            case KEY_W:
+                _index--;
+                if (_index < 0)
+                {
+                    _index = maxNum;
+                }
+                break;
+            case KEY_DOWN:
+            case KEY_S:
+                _index++;
+                if (_index > maxNum)
+                {
+                    _index = 0;
+                }
+                break;
+            case KEY_ENTER:
+            case KEY_SPACE:
+                looping = false;
+                break;
         }
     }
     return _index;
 }
-
 
 int initiateQuiz(vector<Question> _questions)
 {
@@ -286,7 +302,8 @@ int initiateQuiz(vector<Question> _questions)
     int questionIndex = 0;
     history.clear();
 
-    for(Question i : questions) {
+    for (Question i : questions)
+    {
         clear_screen();
         vector<string> choices = i.getChoices();
         int questionNumber = questionIndex + 1;
@@ -294,27 +311,26 @@ int initiateQuiz(vector<Question> _questions)
             interactiveInput(to_string(questionNumber) + ".) " + i.question,
                              choices);
 
-        history.push_back( QuestionHistory(i, answer, choices) );
+        history.push_back(QuestionHistory(i, answer, choices));
         string chosenAnswer = choices[answer];
-        if(chosenAnswer == i.correctAnswer) {
+        if (chosenAnswer == i.correctAnswer)
+        {
             score++;
         }
         questionIndex++;
         cout << endl;
-
     }
 
     return score;
-
 }
 
-
-
-int main() {
+int main()
+{
     system("COLOR 02");
-    vector<string> menuChoices = {"Play","Exit"};
+    vector<string> menuChoices = {"Play", "Exit"};
 
-    switch(interactiveInput("Welcome to Quiz", menuChoices)) {
+    switch (interactiveInput("Welcome to Quiz", menuChoices))
+    {
     case 0:
         break;
     case 1:
@@ -323,21 +339,21 @@ int main() {
 
     clear_screen();
 
-
     acout("What is your name? ");
     getline(cin, playerName);
     bool nameExceededLength = playerName.length() >= 30;
-    playerName = playerName.substr(0,30);
-    if(nameExceededLength) {
+    playerName = playerName.substr(0, 30);
+    if (nameExceededLength)
+    {
         playerName += "...";
     }
 
-
-
-    while(true) {
-        int chosenDifficulty = interactiveInput(playerName + ", please choose the difficulty", vector<string> {"Easy", "Medium", "Hard"});
+    while (true)
+    {
+        int chosenDifficulty = interactiveInput(playerName + ", please choose the difficulty", vector<string>{"Easy", "Medium", "Hard"});
         vector<Question> questionToAnswer;
-        switch(chosenDifficulty) {
+        switch (chosenDifficulty)
+        {
         case 0:
             questionToAnswer = easyQuestions;
             break;
@@ -367,56 +383,58 @@ int main() {
         MSleep(100);
         clear_screen();
 
-        //Render history
+        // Render history
         int index = 0;
-
-        for(QuestionHistory qh : history) {
-
+        for (QuestionHistory qh : history)
+        {
             cout << ++index << ".) " << qh.question << " (" << qh.GetScore() << "/1) point/s\n\n";
-
-            int choiceIndex=0;
-            for(string choice : qh.choices) {
-                cout << choiceIndex+1 << ". " << choice << " ";
-                if(qh.GotCorrectAnswer() && qh.chosenAnswerIndex == choiceIndex) {
+            int choiceIndex = 0;
+            for (string choice : qh.choices)
+            {
+                cout << choiceIndex + 1 << ". " << choice << " ";
+                if (qh.GotCorrectAnswer() && qh.chosenAnswerIndex == choiceIndex)
+                {
                     cout << "(Correctly chosen)";
-                } else {
-                    if(qh.choices[choiceIndex] == qh.correctAnswer ) {
+                }
+                else
+                {
+                    if (qh.choices[choiceIndex] == qh.correctAnswer)
+                    {
                         cout << "(Correct)";
-                    } else if(qh.chosenAnswerIndex == choiceIndex) {
+                    }
+                    else if (qh.chosenAnswerIndex == choiceIndex)
+                    {
                         cout << "(Chosen)";
                     }
                 }
                 cout << endl;
                 choiceIndex++;
                 MSleep(20);
-
             }
             cout << endl;
             MSleep(20);
-
         }
 
-
-        acout (playerName + ", you scored " + to_string(score) + "/" + to_string(questionToAnswer.size()) + " points");
+        acout(playerName + ", you scored " + to_string(score) + "/" + to_string(questionToAnswer.size()) + " points");
         cout << endl << "Press ENTER key to continue" << endl;
-        while(true) {
-            if(_kbhit() && getch() == KEY_ENTER) {
+        while (true)
+        {
+            if (_kbhit() && getch() == KEY_ENTER)
+            {
 
-                break;}
-
+                break;
+            }
         }
         const vector<string> _ = {"Play again", "Quit"};
         int shouldQuit = interactiveInput("Play again?", _);
-        if(shouldQuit == 0) {
+        if (shouldQuit == 0)
+        {
             continue;
         }
         break;
-
     }
 
-
     const string THANK_YOU = R"(
-
 ******** **  **     **     **   ** **   **    **    ** ******* **   **
    **    **  **    *  *    ***  ** **  **      **  **  **   ** **   **
    **    ******   ******   ** * ** *****         **    **   ** **   **
@@ -424,10 +442,10 @@ int main() {
    **    **  ** **      ** **   ** **   **       **    ******* *******
    )";
 
-
-    cout <<endl << endl;
+    cout << endl<< endl;
     auto arr = splitStringToLines(THANK_YOU);
-    for(string s : arr) {
+    for (string s : arr)
+    {
         cout << s;
         MSleep(100);
     }
